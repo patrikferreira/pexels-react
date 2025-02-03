8968f007c9da3ad23965e29f6b885dc71b4d0837
Patrik Ferreira <patrik.andradef@gmail.com>
2025-02-03 22:41:55 +0000

    Search by clicking enter

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -14,6 +14,12 @@ export default function Search() {
         setSearchQuery(query);
     }
 
+    function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+        if (e.key === "Enter") {
+            handleSearch();
+        }
+    }
+
     return <div className="p-2 rounded-2xl flex items-center bg-firstColor">
         <Select />
         <input
@@ -21,6 +27,7 @@ export default function Search() {
             placeholder="Search pictures"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={handleKeyDown}
             className="text-sm font-semibold outline-none px-2 text-foreground bg-firstColor"
         />
         <Button action={handleSearch} content={<IoSearchSharp />} className="hover:bg-background hover:text-foreground transition-all duration-200 p-3 rounded-xl text-xl" />
